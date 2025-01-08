dde2f6ccde176d2961e0407313274b5fcd6a0c8b
pierpaolo1989 <pierpaolo.pdd@gmail.com>
2025-01-08 19:44:23 +0100

    First commit

---
+++ src/test/java/com/soa/ppd_planner/PpdPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soa.ppd_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpdPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
